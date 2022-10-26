09c41c9a79ef86e257a0beedc17614caa6f8d495
SlobodanDodic <slobodandodic@gmail.com>
2022-10-26 12:43:26 +0200

    Local storage

--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,11 +1,20 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import useToggle from "../hooks/useToggle";
 const DataContext = createContext({});
 
 export const DataProvider = ({ children }) => {
   const [conversion, setConversion] = useToggle(true); //Celsius - Fahrenheit
-  const [cities, setCities] = useState([])
   const [city, setCity] = useState(""); // Autocomplete state - typed charaters for search
+
+  const [cities, setCities] = useState(() => {
+    const stored = localStorage.getItem("Cities");
+    const initialValue = JSON.parse(stored);
+    return initialValue || "";
+  });
+
+  useEffect(() => {
+    localStorage.setItem('Cities', JSON.stringify(cities));
+  }, [cities]);
 
   return (
     <DataContext.Provider
